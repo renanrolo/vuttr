78719ecb1e6a72b5472931fb13a367125bde1d8b
Renan Rolo <renanrolo@hotmail.com>
2020-03-02 22:23:20 -0400

    prod baseURL

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api"
+    //baseURL: "http://localhost:3000/api"
+    baseURL: 'https://rolosvuttrapi.herokuapp.com/api'
 });
 
 export default api;
